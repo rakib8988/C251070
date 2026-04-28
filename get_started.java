852ff7ed24b8ac21c85672add6fb71c007f05d4c
rakib8988 <c251070@ugrad.iiuc.ac.bd>
2026-04-29 00:57:15 +0600

    halfpush

---
+++ get_started.java
@@ -0,0 +1,6 @@
+public class get_started {
+    public static void main(String[] args){
+        System.out.println("Java is fun!!!?");
+    }
+    
+}
---
+++ intro.java
@@ -0,0 +1,6 @@
+public class intro{
+	public static void main(String[] args){
+		 String name = "Rakib";
+    System.out.println("Hello " + name);
+	}
+}
